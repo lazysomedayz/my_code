ad81760ba4b63e5f22e30bfb4734d03d1c42659e
Thanawat Srisura <lazysomedayzz@hotmail.com>
2020-11-09 21:39:47 +0700

    Add project files.

---
+++ my_code/ElecEquipment.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace thanawat_sr
+{
+    class ElectricEquipment
+    {
+        protected string brand;
+        protected bool plug;
+        protected bool powerOn;
+        public virtual void TurnOn()
+        {
+            if (plug)
+            {
+                powerOn = true;
+                Console.WriteLine("Now {0} is On!!!", brand);
+            }
+            else
+            {
+                Console.WriteLine("It's can't use without plugged");
+            }
+        }
+        public virtual void TurnOff()
+        {
+            powerOn = false;
+            Console.WriteLine("Now {0} is OFF!!!", brand);
+        }
+    }
+
+    class TV : ElectricEquipment
+    {
+        public TV(bool plugged, string brand)
+        {
+            this.brand = brand;
+            plug = plugged;
+        }
+    }
+
+    class Fan : ElectricEquipment
+    {
+        private int level;
+        public Fan(bool plugged, string brand)
+        {
+            this.brand = brand;
+            plug = plugged;
+            level = 0;
+        }
+
+        public override void TurnOn()
+        {
+        input:
+            Console.Write("Open on what level? [1/2/3] : ");
+            string levelstr = Console.ReadLine();
+            try
+            {
+                level = Int32.Parse(levelstr);
+                if (level > 0 && level <= 3)
+                {
+                    Console.WriteLine("Now The {0} fan is on Level {1}", brand, levelstr);
+                }
+                else
+                {
+                    goto input;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("error");
+                goto input;
+            }
+        }
+        public override void TurnOff()
+        {
+            level = 0;
+            Console.WriteLine("Now {0} is OFF!!!", brand);
+        }
+    }
+}
---
+++ my_code/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace thanawat_sr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool plugged = true;
+            bool unplugged = false;
+
+            TV tv = new TV(unplugged, "LG");
+            tv.TurnOn();
+            tv.TurnOff();
+
+            Fan fan = new Fan(plugged, "HATARI");
+            fan.TurnOn();
+            fan.TurnOff();
+
+            List<ElectricEquipment> eeList = new List<ElectricEquipment>();
+            eeList.Add(new ElectricEquipment());
+            eeList.Add(new TV(plugged, "SAMSUNG"));
+            eeList.Add(new Fan(unplugged, "MISHUBISHI ELEC"));
+
+            foreach (var item in eeList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
